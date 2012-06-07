a7309d5a9f23397068b76933b1357272a5aee5d0
lamecode <ecode@clarifiednetworks.com>
2012-06-08 00:10:23 +0300

    server: Removed Client. Some fixes

--- server.py
+++ server.py
@@ -3,12 +3,6 @@ import uuid
 import socket
 from select import select
 
-class Client:
-    def __init__(self, cid, socket, address):
-        self.cid = cid
-        self.socket = socket
-        self.address = address
-
 class Server:
     def __init__(self, address):
         self.address = address
@@ -16,7 +10,6 @@ class Server:
         self.socket.setblocking(0)
         self.socket.bind(self.address)
         self.running = False
-        self.clients = dict()
         self.sockets = dict()
 
     def descriptors(self):
@@ -27,7 +20,7 @@ class Server:
 
     def generate_id(self):
         cid = str(uuid.uuid4())
-        while cid in self.clients:
+        while cid in self.sockets.values():
             cid = str(uuid.uuid4())
         return cid
 
@@ -43,26 +36,24 @@ class Server:
             for descriptor in r:
                 if descriptor == self.socket:
                     descriptor, address = self.socket.accept()
-                    if len(self.clients) > 3:
+                    if len(self.sockets) > 3:
                         descriptor.send("server full\n")
                         descriptor.close()
                         continue
 
                     cid = self.generate_id()
-                    client = Client(cid, descriptor, address)
-                    print "%s: connected" % client.cid
-                    self.clients[cid] = client
-                    self.sockets[descriptor] = client
+                    print "%s: connected" % cid
+                    self.sockets[descriptor] = cid
                 elif descriptor in self.sockets:
-                    client = self.sockets[descriptor]
+                    cid = self.sockets[descriptor]
 
                     data = descriptor.recv(32)
                     if not data:
-                        print "%s: disconnected" % client.cid
-                        del self.clients[client.cid]
-                        del self.clients[descriptor]
+                        print "%s: disconnected" % cid
+                        del self.sockets[descriptor]
                     else:
                         for descriptor in self.sockets:
+                            descriptor.send(data)
             time.sleep(1/20.0)
 
 

e5fa5f8a343c2217f5ff9b4327f6e75208764da1
lamecode <ecode@clarifiednetworks.com>
2012-06-09 00:17:32 +0300

    spice must flow

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ from cocos.layer import *
 from cocos.sprite import *
 from cocos.actions.interval_actions import MoveTo
 
-from messages import get_unpacker, pack_position, pack_eat, pack_dead
+from messages import get_unpacker, pack_keyup, pack_keydown
 
 LEFT = 65361
 UP = 65362
@@ -52,6 +52,7 @@ class ServerConnection(object):
     def write(self, data):
         if data != self.last:
             self.last = data
+            print "ding", repr(data)
             self.socket.send(data)
 
 class Player(Sprite):
@@ -64,12 +65,11 @@ class Player(Sprite):
 class PlayerLayer(ScrollableLayer):
     is_event_handler = True
 
-    def __init__(self, width, height, start_position):
+    def __init__(self, width, height):
         super(PlayerLayer, self).__init__()
         self.px_width = width
         self.px_height = height
         
-        self.chars_pressed = set()
         self.schedule(self.update)
         self.schedule_interval(self.update_network, 1/20.0)
         
@@ -80,10 +80,25 @@ class PlayerLayer(ScrollableLayer):
         self.players = dict()
 
     def on_key_press(self, key, modifiers):
-        self.chars_pressed.add(key)
+        print key
+        if key == LEFT:
+            serverConnection.write(pack_keydown(MOVELEFT))
+        elif key == RIGHT:
+            serverConnection.write(pack_keydown(MOVERIGHT))
+        elif key == UP:
+            serverConnection.write(pack_keydown(MOVEUP))
+        elif key == DOWN:
+            serverConnection.write(pack_keydown(MOVEDOWN))
 
     def on_key_release(self, key, modifiers):
-        self.chars_pressed.remove(key)
+        if key == LEFT:
+            serverConnection.write(pack_keyup(MOVELEFT))
+        elif key == RIGHT:
+            serverConnection.write(pack_keyup(MOVERIGHT))
+        elif key == UP:
+            serverConnection.write(pack_keyup(MOVEUP))
+        elif key == DOWN:
+            serverConnection.write(pack_keyup(MOVEDOWN))
 
     def update(self, dt):
         player = self.players.get(self.cid, None)
@@ -92,32 +107,10 @@ class PlayerLayer(ScrollableLayer):
             self.get_ancestor(ScrollingManager).set_focus(int(x), int(y))
 
     def update_network(self, dt):
-        #read input
-        self.movedir = 0
-        player = self.players.get(self.cid, None)
-        if player:
-            x, y = player.target
-            if LEFT in self.chars_pressed:
-                self.movedir = MOVELEFT
-                x -= 200 * dt
-            if UP in self.chars_pressed:
-                self.movedir = MOVEUP
-                y += 200 * dt
-            if RIGHT in self.chars_pressed:
-                self.movedir = MOVERIGHT
-                x += 200 * dt
-            if DOWN in self.chars_pressed:
-                self.movedir = MOVEDOWN
-                y -= 200 * dt
-            player.target = (x, y) 
-
-            if self.get_ancestor(GameLevelScene).movePlayer(self.movedir)  is not None:
-                print("")
-                #position = pack_position(self.cid, 1, x, y)
-                #serverConnection.write(position)
-
         #read networkstuff
         mid, data = serverConnection.read()
+        if mid:
+            print repr(mid), repr(data)
         if mid == 1:
             self.cid = data
             player = Player(self.cid, "test.png", (320,240))
@@ -127,7 +120,7 @@ class PlayerLayer(ScrollableLayer):
             cid, direction, x, y = data
             player = self.players.get(cid, None)
             if not player:
-                player = Player(cid, "test.png", (320,240))
+                player = Player(cid, "test.png", (x,y))
                 self.add(player)
                 self.players[cid] = player
 
@@ -136,88 +129,24 @@ class PlayerLayer(ScrollableLayer):
             player.moveto = MoveTo((x,y), 1/20.0)
             player.do(player.moveto)
 
-        #write position
-        player = self.players.get(self.cid, None)
-        if player:
-            x, y = player.target 
-            position = pack_position(self.cid, 1, x, y)
-            serverConnection.write(position)
-
+#        #write position
+#        player = self.players.get(self.cid, None)
+#        if player:
+#            x, y = player.target 
+#            position = pack_position(self.cid, 1, x, y)
+#            serverConnection.write(position)
 
 class GameLevelScene(Scene):
     def __init__(self):
         super(GameLevelScene, self).__init__()
         self.scroller = ScrollingManager()
         bglayer = ScrollableLayer()
-        f = open('level1.txt', "r")
-        self.worldGrid = []
-        for line in f:
-            line = line.replace('\n','')
-            self.worldGrid.append(list(line))
         bglayer.add(Sprite('background.png', position=(1300, 360)))
         self.scroller.add(bglayer)
-        for sublist in self.worldGrid:
-            for char in sublist:
-                if char == 'A':
-                    self.scroller.add(PlayerLayer(2600, 720, self.getPlayerPosition()), z=1)
+        self.scroller.add(PlayerLayer(2600, 720), z=1)
         self.add(self.scroller)
         
-    def isMoveLegal(self, movedir):
-        position = self.getPlayerPosition()
-        #print(position)
-        try:
-            if movedir == MOVELEFT:
-                if self.worldGrid[position[1]][position[0]-1] == '1' or position[0] == 0:
-                    return False
-                else:
-                    return True
-            elif movedir == MOVEUP:
-                if self.worldGrid[position[1]-1][position[0]] == '1' or position[1] == 0:
-                    return False
-                else:
-                    return True
-            elif movedir == MOVERIGHT:
-                if self.worldGrid[position[1]][position[0]+1] == '1': #or position[0] == len(self.worldGrid[position[1]])-1:
-                    return False
-                else:
-                    return True
-            elif movedir == MOVEDOWN:
-                if self.worldGrid[position[1]+1][position[0]] == '1':# or position[1] == len(self.worldGrid)-1:
-                    return False
-                else:
-                    return True
-            else:
-                return False
-        except IndexError:
-            return False
-        
-    def movePlayer(self, movedir):
-        position = self.getPlayerPosition()
-        if self.isMoveLegal(movedir):
-            if movedir == MOVELEFT:
-                self.worldGrid[position[1]][position[0]] = '0'
-                self.worldGrid[position[1]][position[0]-1] = 'A'
-            if movedir == MOVEUP:
-                self.worldGrid[position[1]][position[0]] = '0'
-                self.worldGrid[position[1]-1][position[0]] = 'A'
-            if movedir == MOVERIGHT:
-                self.worldGrid[position[1]][position[0]] = '0'
-                self.worldGrid[position[1]][position[0]+1] = 'A'
-            if movedir == MOVEDOWN:
-                self.worldGrid[position[1]][position[0]] = '0'
-                self.worldGrid[position[1]+1][position[0]] = 'A'
-        else:
-            return
-        
-        
-    def getPlayerPosition(self):
-        for sublist in self.worldGrid:
-            for char in sublist:
-                if char == 'A':
-                    return (sublist.index(char),self.worldGrid.index(sublist))
-        
-
 if __name__ == '__main__':
-    serverConnection = ServerConnection('shell.jkry.org', 10066)
+    serverConnection = ServerConnection('', 10066)
     director.init(width=1240, height=720)
     director.run(GameLevelScene())
--- server.py
+++ server.py
@@ -2,7 +2,20 @@ import time
 import random
 import socket
 from select import select
-from messages import get_unpacker, pack_cid, pack_server_full
+from messages import get_unpacker, pack_cid, pack_server_full, pack_position
+
+MOVELEFT = 1
+MOVERIGHT = 3
+MOVEUP = 2
+MOVEDOWN = 4
+
+
+class Player:
+    def __init__(self, cid, position):
+        self.cid = cid
+        self.position = position
+        self.direction = 1
+        self.velocity = 0
 
 class Server:
     def __init__(self, address):
@@ -13,6 +26,17 @@ class Server:
         self.socket.bind(self.address)
         self.running = False
         self.sockets = dict()
+        self.players = dict()
+
+        f = open('level1.txt', "r")
+        self.worldGrid = []
+        for line in f:
+            line = line.replace('\n','')
+            self.worldGrid.append(list(line))
+
+        self.start_points = dict()
+        for cid in ["A"]: #,"B","C","D"]:
+            self.start_points[cid] = self.getPlayerPosition(cid)
 
     def descriptors(self):
         yield self.socket
@@ -21,9 +45,10 @@ class Server:
             yield descriptor
 
     def generate_id(self):
-        cid = chr(random.randint(65,90))
+        return 'A'
+        cid = chr(random.randint(65,68))
         while cid in self.sockets.values():
-            cid = chr(random.randint(65,90))
+            cid = chr(random.randint(65,68))
         return cid
 
     def run(self):
@@ -45,33 +70,96 @@ class Server:
 
                     cid = self.generate_id()
                     print "%s: connected" % cid
-                    self.sockets[descriptor] = cid
+                    position = self.start_points["A"] 
+                    player = Player(cid, position)
+                    self.sockets[descriptor] = player
                     descriptor.send(pack_cid(cid))
                 elif descriptor in self.sockets:
-                    cid = self.sockets[descriptor]
+                    player = self.sockets[descriptor]
 
-                    data = None
+                    mid, data = None, None
                     try:
                         mid = descriptor.recv(1)
                         if mid:
+                            print repr(mid)
                             length, unpacker = get_unpacker(mid)
                             if length:
-                                data = mid + descriptor.recv(length)
+                                data = descriptor.recv(length)
+                                if ord(mid) == 6: #key up
+                                    key = unpacker(data)
+                                    player.velocity = 0
+                                elif ord(mid) == 7: #key down
+                                    key = unpacker(data)
+                                    print "KEY", key
+                                    player.direction = key
+                                    player.velocity = 1
                     except socket.error, err:
                         print "socket.error", repr(err)
 
-                    if not data:
-                        print "%s: disconnected" % cid
-                        del self.sockets[descriptor]
-                    else:
-                        for descriptor in self.sockets:
-                            try:
-                                start = time.time()
-                                descriptor.send(data)
-                            except socket.error, err:
-                                print "socket.error", repr(err)
-                                del self.sockets[descriptor]
+            for descriptor in self.sockets:
+                player = self.sockets[descriptor]
+                if player.velocity:
+                    if self.movePlayer(player, player.direction):
+                        print "bar"
+                        for foo in self.sockets:
+                            descriptor.send(pack_position(player.cid, 
+                                                          player.direction,
+                                                          player.position[0],
+                                                          player.position[1]))
+
+    def movePlayer(self, player, movedir):
+        position = self.getPlayerPosition(player.cid)
+        if self.isMoveLegal(player, movedir):
+            if movedir == MOVELEFT:
+                self.worldGrid[position[1]][position[0]] = '0'
+                self.worldGrid[position[1]][position[0]-1] = player.cid
+            if movedir == MOVEUP:
+                self.worldGrid[position[1]][position[0]] = '0'
+                self.worldGrid[position[1]-1][position[0]] = player.cid
+            if movedir == MOVERIGHT:
+                self.worldGrid[position[1]][position[0]] = '0'
+                self.worldGrid[position[1]][position[0]+1] = player.cid
+            if movedir == MOVEDOWN:
+                self.worldGrid[position[1]][position[0]] = '0'
+                self.worldGrid[position[1]+1][position[0]] = player.cid
+            return True
+        return False
+
+    def isMoveLegal(self, player, movedir):
+        position = self.getPlayerPosition(player.cid)
+        #print(position)
+        try:
+            if movedir == MOVELEFT:
+                if self.worldGrid[position[1]][position[0]-1] == '1' or position[0] == 0:
+                    return False
+                else:
+                    return True
+            elif movedir == MOVEUP:
+                if self.worldGrid[position[1]-1][position[0]] == '1' or position[1] == 0:
+                    return False
+                else:
+                    return True
+            elif movedir == MOVERIGHT:
+                if self.worldGrid[position[1]][position[0]+1] == '1': #or position[0] == len(self.worldGrid[position[1]])-1:
+                    return False
+                else:
+                    return True
+            elif movedir == MOVEDOWN:
+                if self.worldGrid[position[1]+1][position[0]] == '1':# or position[1] == len(self.worldGrid)-1:
+                    return False
+                else:
+                    return True
+            else:
+                return False
+        except IndexError:
+            return False
+
+    def getPlayerPosition(self, cid):
+        for sublist in self.worldGrid:
+            for char in sublist:
+                if char == cid:
+                    return (sublist.index(char),self.worldGrid.index(sublist))
 
 if __name__ == "__main__":
-    s = Server(("", 6660))
+    s = Server(("", 10066))
     s.run()
